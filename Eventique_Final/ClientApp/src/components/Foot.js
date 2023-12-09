093172f9d1ad4aec45504add2bb290ab8caed195
Julius <test@test.com>
2023-12-09 08:41:32 -0400

    Foot and Home layout

---
+++ Eventique_Final/ClientApp/src/components/Foot.js
@@ -0,0 +1,20 @@
+﻿
+    export default function Foot() {
+        return (
+            <div className='post'>
+                <div className='image'>
+                    <img src="https://techcrunch.com/wp-content/uploads/2023/05/GettyImages-1185699748.jpg?w=1390&crop=1" />
+                </div>
+                <div className='texts'>
+                    <h2>Eventique is bringing huge to people</h2>
+                    <p className='info'>
+                        <a className='author'>Di Sun</a>
+                        <time>2023-08-21 16:50</time>
+                    </p>
+                    <p className='summary'>
+                        text here introduction
+                    </p>
+                </div>
+            </div>
+        )
+    }
--- Eventique_Final/ClientApp/src/components/Home.js
+++ Eventique_Final/ClientApp/src/components/Home.js
@@ -18,13 +18,26 @@ const Home = () => {
   
 
   return (
-    <div>
+      <div>
+          <header>
+              <Link to="/" className="logo">
+                  MyBlog
+              </Link>
+              <nav>
+                  <Link to="/login">Login</Link>
+                  <Link to="/register">Register</Link>
+                  <a href="">About</a>
+              </nav>
+          </header>
     {eventData && eventData.map((event, index) => (
         <div key={index}>
             <h2>Event Name: {event.evenT_NAME}</h2>
             <p>Date: {event.evenT_DATE}</p>
             <p>Category: {event.evenT_CATEGORY}</p>
             <Link to={`/event_details/${event.evenT_ID}`}><button>More Details</button></Link>
+            <div>
+            <img>Mush it with other details</img>
+            </div>
         </div>
     ))}
 </div>
